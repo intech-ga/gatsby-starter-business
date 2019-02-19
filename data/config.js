4b4c7a7126ac1e737447915bb25a4b0ef2aaac6a
intech-ga <47254104+intech-ga@users.noreply.github.com>
2019-02-19 07:32:03 -0500

    update site name and colors

--- data/config.js
+++ data/config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  siteTitle: 'Gatsby Starter Business', // Site title.
+  siteTitle: 'Fluid Websites', // Site title.
   siteTitleAlt: 'Business', // Alternative site title for SEO.
   siteLogo: '/icons/icon-512x512.png', // Logo used for SEO and manifest.
-  siteUrl: 'https://gatsby-starter-business.netlify.com', // Domain of your website without pathPrefix.
+  siteUrl: 'https://fluidwebsites.ml', // Domain of your website without pathPrefix.
   pathPrefix: '/', // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-business/.
-  siteDescription: 'Leverage Gatsby Business Starter for your Business.', // Website description used for RSS feeds/meta description tag.
+  siteDescription: 'Growth driven, responsive website design.', // Website description used for RSS feeds/meta description tag.
   siteRss: '/rss.xml',
   siteFBAppID: '', // FB Application ID for using app insights
   googleTagManagerID: '', // GTM tracking ID.
@@ -13,7 +13,7 @@ module.exports = {
   userTwitter: 'alaprone',
   userLocation: 'Sebring, FL',
   userDescription: '',
-  copyright: 'Copyright © Gatsby Starter Business 2018. All Rights Reserved.', // Copyright string for the footer of the website and RSS feed.
-  themeColor: '#00d1b2', // Used for setting manifest and progress theme colors.
+  copyright: 'Copyright © FluidWebsites.ml 2019. All Rights Reserved.', // Copyright string for the footer of the website and RSS feed.
+  themeColor: '#9b4bd8', // Used for setting manifest and progress theme colors.
   backgroundColor: '#ffffff', // Used for setting manifest background color.
 }
