02932a17ee40ab2a05bee9c5adf97ea15d83fe83
intech-ga <47254104+intech-ga@users.noreply.github.com>
2019-02-19 07:16:06 -0500

    Update user info

--- data/config.js
+++ data/config.js
@@ -9,9 +9,9 @@ module.exports = {
   siteFBAppID: '', // FB Application ID for using app insights
   googleTagManagerID: '', // GTM tracking ID.
   disqusShortname: 'gatsby-business-starter', // Disqus shortname.
-  userName: 'Vaibhav Sharma',
-  userTwitter: 'vaibhaved',
-  userLocation: 'Delhi NCR, India',
+  userName: 'alaprone',
+  userTwitter: 'alaprone',
+  userLocation: 'Sebring, FL',
   userDescription: '',
   copyright: 'Copyright © Gatsby Starter Business 2018. All Rights Reserved.', // Copyright string for the footer of the website and RSS feed.
   themeColor: '#00d1b2', // Used for setting manifest and progress theme colors.
